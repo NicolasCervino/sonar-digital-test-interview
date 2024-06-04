74e8ed9dbbbe02f10664362382960c617ac30b6b
Nicolas Cervino <nicolascervino2@gmail.com>
2024-06-03 21:51:53 -0300

    backend ready

---
+++ backend/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express()
+
+const PORT = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Hello World!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
+}).on("error", (error) => {
+    throw new Error(error.message);
+})
+
+
