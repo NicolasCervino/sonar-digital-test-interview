e57eddbf1d30fae180b2b4e54f51d7bd8e4476bd
Nicolas Cervino <nicolascervino2@gmail.com>
2024-06-03 22:06:02 -0300

    added eslint config

--- backend/index.ts
+++ backend/index.ts
@@ -2,18 +2,18 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('Hello World!')
+    res.status(200).send('Hello World!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
+    console.log(`Listening on port: ${PORT}`);
 }).on("error", (error) => {
     throw new Error(error.message);
-})
+});
 
 
